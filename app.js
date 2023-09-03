844133fa8e114a0cf3f997ce824bffdb73be537e
Syed Muhammad Aizaz Ali <136581038+syedMuhammadAizazALi@users.noreply.github.com>
2023-09-03 10:02:45 -0700

    colorchange

---
+++ app.js
@@ -0,0 +1,45 @@
+
+
+const colors = ["Green","Red","Blue","Purple"];
+
+
+function changeColor(colors)
+{
+    if (colors === "Green") {
+        document.body.style.backgroundColor='green'
+    } 
+    else if (colors === "Red"){
+        document.body.style.backgroundColor='red'
+    }    
+
+    else if (colors === "Blue"){
+        document.body.style.backgroundColor='blue'
+    }
+    else if (colors === "Purple"){
+        document.body.style.backgroundColor='purple'
+    }
+   
+    
+}
+
+
+
+
+// const colors = ["Green", "Red", "Blue", "Purple"];
+
+// function changeColor(color) {
+//   if (color === "Green") {
+//     document.body.style.backgroundColor = 'green';
+//   } 
+//   else if (color === "Red") {
+//     document.body.style.backgroundColor = 'red';
+//   } 
+//   else if (color === "Blue") {
+//     document.body.style.backgroundColor = 'blue';
+//   } 
+//   else if (color === "Purple") {
+//     document.body.style.backgroundColor = 'Purple';
+//   }
+// }
+
+
